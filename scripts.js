c46e62ae1681895dadd7dc845819af5bfc8f65a4
Behara Vikram <148604650+beharavikram@users.noreply.github.com>
2024-07-24 19:21:34 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,76 @@
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval;
+
+function timeToString(time) {
+  let diffInHrs = time / 3600000;
+  let hh = Math.floor(diffInHrs);
+
+  let diffInMin = (diffInHrs - hh) * 60;
+  let mm = Math.floor(diffInMin);
+
+  let diffInSec = (diffInMin - mm) * 60;
+  let ss = Math.floor(diffInSec);
+
+  let diffInMs = (diffInSec - ss) * 100;
+  let ms = Math.floor(diffInMs);
+
+  let formattedHH = hh.toString().padStart(2, "0");
+  let formattedMM = mm.toString().padStart(2, "0");
+  let formattedSS = ss.toString().padStart(2, "0");
+  let formattedMS = ms.toString().padStart(2, "0");
+
+  return ${formattedHH}:${formattedMM}:${formattedSS}:${formattedMS};
+}
+
+function print(txt) {
+  document.getElementById("display").innerHTML = txt;
+}
+
+function start() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(() => {
+    elapsedTime = Date.now() - startTime;
+    print(timeToString(elapsedTime));
+  }, 10);
+  showButton("PAUSE");
+}
+
+function pause() {
+  clearInterval(timerInterval);
+  showButton("PLAY");
+}
+
+function reset() {
+  clearInterval(timerInterval);
+  print("00:00:00:00");
+  elapsedTime = 0;
+  
+  document.getElementById("laps").innerHTML = "";
+  showButton("PLAY");
+}
+
+function lap() {
+  let lapTime = timeToString(elapsedTime);
+  let li = document.createElement("li");
+  li.innerText = lapTime;
+  document.getElementById("laps").appendChild(li);
+}
+
+function showButton(buttonKey) {
+  const buttonToShow = buttonKey === "PLAY" ? startButton : pauseButton;
+  const buttonToHide = buttonKey === "PLAY" ? pauseButton : startButton;
+  buttonToShow.style.display = "block";
+  buttonToHide.style.display = "none";
+}
+
+// Event listeners
+let startButton = document.getElementById("start");
+let pauseButton = document.getElementById("pause");
+let resetButton = document.getElementById("reset");
+let lapButton = document.getElementById("lap");
+
+startButton.addEventListener("click", start);
+pauseButton.addEventListener("click", pause);
+resetButton.addEventListener("click", reset);
+lapButton.addEventListener("click", lap);
